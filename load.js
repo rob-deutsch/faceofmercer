7a6a2a4c34e549a1203bd21f552a7fe96c930367
rob <rob@rob.rob>
2015-07-18 08:35:46 -0400

    Now taking screenshot after changing slide

--- load.js
+++ load.js
@@ -3,7 +3,12 @@ page.viewportSize = { width: 1280, height: 1024 };
 page.open('http://www.mercer.com.au', function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
-		page.render('example.png');
+		page.evaluate(function() {
+			$(".full-width-carousel .slider").slickPause();
+			$(".full-width-carousel .slider").slickGoTo(3, false);
+		});
+		window.setTimeout(function() {page.render('homepage.png');}, 3000);
+		//page.render('homepage.png');
 	}
-	phantom.exit();
+	//phantom.exit();
 });

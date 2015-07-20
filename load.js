21c8dfe1a5ac5f9a49e0ada1fd2d967a5570bf8c
rob <rob@rob.rob>
2015-07-20 04:28:48 -0400

    Slightly broken for non-main slider

--- load.js
+++ load.js
@@ -5,31 +5,49 @@ function sleep(miliseconds) {
            }
 };
 
-function captureSlide(page, number) {
-	var sliderWidth = page.evaluate(function() {
-		return $(".full-width-carousel .slider .slick-track").width();
-	});
-	var sliderCount = page.evaluate(function() {
-		var mainCarousel = $(".full-width-carousel .slider").getSlick();
+function captureSlide(page, carouselClass, number) {
+	var sliderSelector = "." + carouselClass + " .slider";
+	var trackSelector = sliderSelector + " .slick-track";
+
+	var sliderWidth = page.evaluate(function(trackSelector) {
+		return $(trackSelector).width();
+	}, trackSelector);
+
+	var sliderCount = page.evaluate(function(sliderSelector) {
+		var mainCarousel = $(sliderSelector).getSlick();
 		return mainCarousel.slideCount;
-	});
+	}, sliderSelector);
+
 	var offset = (number + 1) * -(sliderWidth / (sliderCount + 2));
-	page.evaluate(function(offset) {
-		$(".full-width-carousel .slider").slickSetOption("dots", false, true);
-		$(".full-width-carousel .slider").slickPause();
-		$(".full-width-carousel .slider .slick-track").css({"transform": "translate3d(" + offset + "px, 0px, 0px)"});
-	}, offset);
-	var clipRect = page.evaluate(function() {
-		return document.querySelector(".full-width-carousel .slider").getBoundingClientRect();
-	});
+	page.evaluate(function(offset, sliderSelector, trackSelector) {
+		$(sliderSelector).slickSetOption("dots", false, true);
+		$(sliderSelector).slickPause();
+		$(trackSelector).css({"transform": "translate3d(" + offset + "px, 0px, 0px)"});
+	}, offset, sliderSelector, trackSelector);
+	var clipRect = page.evaluate(function(sliderSelector) {
+		return document.querySelector(sliderSelector).getBoundingClientRect();
+	}, sliderSelector);
 	page.clipRect = {
 		top: clipRect.top,
 		left: clipRect.left,
 		width: clipRect.width,
 		height: clipRect.height
 	};
-	page.render(number + '.png');
-	console.log("Saved image " + number);
+	var filename = carouselClass + number + ".png";
+	page.render(filename);
+	console.log("Saved image " + filename);
+};
+
+function captureSlides(page, carouselClass) {
+	var sliderSelector = "." + carouselClass + " .slider";
+	var slideCount = page.evaluate(function(sliderSelector) {
+		var mainCarousel = $(sliderSelector).getSlick();
+		return mainCarousel.slideCount;
+	}, sliderSelector);
+	console.log(carouselClass + " slide count: " + slideCount);
+	for(var i=0; i < slideCount; i++) {
+		captureSlide(page,carouselClass,i);
+	};
 };
 
 var page = require('webpage').create();
@@ -39,14 +57,8 @@ page.open('http://www.mercer.com', function(status) {
 	if(status === "success") {
 		page.render('homepage.png');
 		console.log("Saved homepage.png");
-		var slideCount = page.evaluate(function() {
-			var mainCarousel = $(".full-width-carousel .slider").getSlick();
-			return mainCarousel.slideCount;
-		});
-		console.log("slide count: " + slideCount);
-		for(var i=0; i < slideCount; i++) {
-			captureSlide(page,i);
-		};		
+		captureSlides(page, "full-width-carousel");
+		captureSlides(page, "promo-slider");
 	}
 	phantom.exit();
 });

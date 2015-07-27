b19e6db1588451567563f9b03dad8f1182fe97ed
Rob Deutsch <rob-deutsch@users.noreply.github.com>
2015-07-28 00:15:32 +0900

    Compressed page.evaluates

--- load.js
+++ load.js
@@ -21,33 +21,31 @@ function captureSlides(page, name, carouselSelector) {
 	var hashes = [];
 	
 	for (var number = 0; number < sliderCount; number++) {
-	// Calculate the per-slide pixel offset
-	// Assumed that half of cloned are first, and half are last
-	var offset = (number + clonedCount / 2) * -(sliderWidth / (sliderCount + clonedCount));
+		// Calculate the per-slide pixel offset
+		// Assumed that half of cloned are first, and half are last
+		var offset = (number + clonedCount / 2) * -(sliderWidth / (sliderCount + clonedCount));
+		
+		// Move the requested slide to be right in the middle
+		// And get the exact location of the slide on the page
+		var clipRect = page.evaluate(function(offset, carouselSelector, trackSelector) {
+			$(carouselSelector).slickSetOption("dots", false, true);
+			$(carouselSelector).slickPause();
+			$(trackSelector).css({"transform": "translate3d(" + offset + "px, 0px, 0px)"});
 	
-	// Move the requested slide to be right in the middle
-	page.evaluate(function(offset, carouselSelector, trackSelector) {
-		$(carouselSelector).slickSetOption("dots", false, true);
-		$(carouselSelector).slickPause();
-		$(trackSelector).css({"transform": "translate3d(" + offset + "px, 0px, 0px)"});
-	}, offset, carouselSelector, trackSelector);
-	
-	// Get the exact location of the slide on the page
-	var clipRect = page.evaluate(function(trackSelector, i) {
-		slide = $(trackSelector + " .slick-slide").not(".slick-cloned")[i]
-		return slide.getBoundingClientRect();
-	}, trackSelector, number);
-	
-	// Set the clipping to only be equal to the slide
-	page.clipRect = {
-		top: clipRect.top,
-		left: clipRect.left,
-		width: clipRect.width,
-		height: clipRect.height
-	};
-	
-	// Save the image
-	hashes.push(page.renderBase64('PNG'));
+			slide = $(trackSelector + " .slick-slide").not(".slick-cloned")[i]
+			return slide.getBoundingClientRect();
+		}, offset, carouselSelector, trackSelector);
+		
+		// Set the clipping to only be equal to the slide
+		page.clipRect = {
+			top: clipRect.top,
+			left: clipRect.left,
+			width: clipRect.width,
+			height: clipRect.height
+		};
+		
+		// Save the image
+		hashes.push(page.renderBase64('PNG'));
 	}
 	return(hashes);
 };

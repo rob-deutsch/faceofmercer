6ee9b45c59a5660915edf315eff4b9895b28b165
Rob Deutsch <rob-deutsch@users.noreply.github.com>
2015-07-30 01:21:25 +0900

    load.js to get list of sites

--- load.js
+++ load.js
@@ -104,6 +104,25 @@ function captureFeaturedArticles(page) {
 var page = require('webpage').create();
 page.viewportSize = { width: 1024, height: 768 };
 page.open('http://www.mercer.com', function(status) {
+	var sites = page.evaluate(function () {
+		var countries = $(".mrc-country").find("p:has(a)");
+		var sites = {};
+		countries.each(function(index) {
+			countryName = $(this).clone().children().remove().end().text().trim();
+			var langs = {};
+			$(this).find("a").each(function(index) {
+				var lang = $(this).text().trim();
+				var url = $(this).get(0).href;
+				langs[lang] = url;
+			});
+			sites[countryName] = langs;
+		});
+		return(sites);
+	});
+	console.log(sites);
+});
+
+page.open('http://www.mercer.com', function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
 		console.log("Saving home page");

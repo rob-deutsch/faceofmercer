a9830b04eaabe570ce22315f83c8e3b517c02681
rob <rob@rob.rob>
2015-08-03 03:39:12 -0400

    Extract.js will upload to S3

--- extract.js
+++ extract.js
@@ -5,6 +5,7 @@ var exec = require('child_process').exec,
 AWS.config.loadFromPath('./credentials_aws.json');
 var s3 = new AWS.S3({params: {Bucket: 'faceofmercer'} });
 
+var timestamp = '201508031300';
 var country = "Australia";
 var lang = "English";
 var url = "http://www.mercer.com.au";
@@ -20,19 +21,31 @@ exec(get_site_cmd, function(err, stdout, stderr) {
 		if (site.slides.hasOwnProperty(section)) {
 			for (var slide in site.slides[section]) {
 				if (site.slides[section].hasOwnProperty(slide)) {
-					slide = site.slides[section][slide];
-					uploadSlideToS3(slide);
+					uploadSlideToS3(site.slides, section, slide);
 				}
 			}
 		}
 	};
+	uploadToS3(timestamp + 
+	           '/' + country + '-' + lang + 
+                   '/homepage.png', site.screenshot);
 });
 
-function uploadSlideToS3(slide) {
+function uploadSlideToS3(slides, section, slide) {
+	//var filename = Math.floor(Math.random()*10000).toString() + '.png';
+	var filename = timestamp + 
+	               '/' + country + '-' + lang + 
+                       '/' + section + 
+	               '/' + slide + '.png';
+	var data = slides[section][slide].screenshot;
+	uploadToS3(filename, data);
+};
+
+function uploadToS3(filename, data) {
 	var buf = new Buffer(data, 'base64');
 	var params = {
-		Key: Math.floor(Math.random()*10000).toString() + '.png',
-		Body: slide.screenshot,
+		Key: filename,
+		Body: buf,
 		ContentEncoding: 'base64',
 		ContentType: 'image/png',
 	};

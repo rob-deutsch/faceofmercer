ca00ddc260670024231c2186cddfc72e656deb1c
rob <rob@rob.rob>
2015-08-03 10:13:35 -0400

    Sql.js use SSL now

--- sql.js
+++ sql.js
@@ -1,10 +1,17 @@
-var mysql      = require('mysql');
-var credentials     = require('./credentials');
+var mysql       = require('mysql');
+var credentials = require('./credentials');
+var fs          = require('fs');
+
 var connection = mysql.createConnection({
   host     : credentials.host,
   user     : credentials.user,
   password : credentials.password,
   database : credentials.database,
+  ssl      : {
+    ca   : fs.readFileSync(__dirname + '/server-ca.pem'),
+    cert : fs.readFileSync(__dirname + '/client-cert.pem'),
+    key  : fs.readFileSync(__dirname + '/client-key.pem')
+  }
 });
 
 connection.connect(function(err) {
